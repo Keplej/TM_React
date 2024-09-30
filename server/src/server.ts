bfb49bb644eeea42605e3bb7b5cf458e3a4033f2
Keplej <Johnny.Keple@gmail.com>
2024-09-30 14:47:42 -0500

    making changes to add basic client server communication via sockets

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,28 @@
-import express, { Request, Response } from 'express';
+import { Server } from "socket.io";
+import express from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+const server = createServer(app);
 const port = 3000;
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
+})
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('<h1>Hello World</h1>');
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+io.on("connection", (socket) => {
+    console.log(socket.id);
+})
+
+server.listen(port, () => {
+    console.log(`server is running at http://localhost:${port}`)
+})
